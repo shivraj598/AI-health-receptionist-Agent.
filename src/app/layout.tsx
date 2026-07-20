f72fcd647a150cb8f9af0a91b6df7c77f7fa7026
shivraj.dev <timilsenashivraj598@gmail.com>
2026-07-20 14:13:04 +0545

    feat: enhance layout with TooltipProvider and AuthProvider integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import '@/styles/globals.css'
+import { AuthProvider } from '@/providers/AuthProvider'
+import { TooltipProvider } from '@/components/ui/tooltip'
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="dark">
+      <body>
+        <TooltipProvider>
+          <AuthProvider>
+            {children}
+          </AuthProvider>
+        </TooltipProvider>
+      </body>
     </html>
   )
 }
